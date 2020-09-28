b70428ec836f71c471102d2a976a99c08a94dbfe
Gilvan Carlos Teixeira Junior <49037876+juniorteixeira1805@users.noreply.github.com>
2020-09-27 23:15:00 -0300

    Inicio do Projeto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+app.use.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+
+app.listen(3000)
