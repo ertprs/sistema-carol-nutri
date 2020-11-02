c3c7848d761bd9cd5668565e748a87f695e65eb8
Gilvan Carlos Teixeira Junior <49037876+juniorteixeira1805@users.noreply.github.com>
2020-11-01 21:47:31 -0300

    adicionado o cors

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -5,9 +5,10 @@ const cors = require('cors')
 
 
 const app = express()
-app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cors())
+
 
 //--Rotas
 app.use('/user', require('./app/routers/user'))
