02a722f76eacabba9439a3c117d8537de22c307a
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-01-03 23:52:06 -0300

    mudanças na dashboard

--- front-end/src/pages/dashboard/index.js
+++ front-end/src/pages/dashboard/index.js
@@ -1,24 +1,11 @@
-import React, {useContext} from 'react';
-
-import {AuthContext} from '../../context/AuthContext'
-
-import './styles.css'
+import React from 'react';
 
 export default function Dashboard(){
 
-    const { user } = useContext(AuthContext)
-
-    if(user.eAdmin === true){
         return (
             <>
-                <p>é adm</p>
+                <p>Hellow world </p>
             </>
         )
-    } else {
-        return (
-            <>
-                <p>Não é adm</p>
-            </>
-        )
-    }
+    
 }
