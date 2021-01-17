6f71ac13011be7cfcf9547c963628e36fa85587a
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-01-17 17:35:54 -0300

    função para a sessão expirar

--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -1,22 +1,43 @@
-import React, { createContext, useCallback, useState } from "react"
+import React, { createContext, useCallback, useEffect, useState, useRef } from "react"
 import api from '../services/api'
 import {toast} from 'react-toastify'
 
 export const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
-const [data, setData] = useState(() => {
-    const token = localStorage.getItem('@CarolNutri:token')
-    var user = localStorage.getItem('@CarolNutri:user')
+    const [data, setData] = useState(() => {
+        const token = localStorage.getItem('@CarolNutri:token')
+        var user = localStorage.getItem('@CarolNutri:user')
 
-    api.defaults.headers['authorization'] = `Bearer ${token}`
+        api.defaults.headers['authorization'] = `Bearer ${token}`
 
-    if(token && user){
-        return { token: token, user: JSON.parse(user)}
-    }
+        if(token && user){
+            return { token: token, user: JSON.parse(user)}
+        }
 
-    return {}
-})
+        return {}
+    })
+
+
+    const cb = useRef() //serve para ser a causa de disparo do usuEffect quando  este Component é gerado, ou seja, useEffect será chamado quando o usuário entrar nesta página.
+
+    useEffect(() => {
+        toast.info(`Seu tempo de seu tempo de sessão irá expirar em 4 horas.`)
+
+        setTimeout(function(){ 
+            localStorage.removeItem('@CarolNutri:token')
+            localStorage.removeItem('@CarolNutri:user')
+        
+            setData({})
+        }, 14400000);
+
+        setTimeout(function(){ 
+            toast.info(`Seu tempo de seu tempo de sessão irá expirar em 30 minutos.`)
+        }, 12600000);
+    },[cb])
+
+
+
 
 const signIn = useCallback( async ({ email, password}) => {
     try {
@@ -33,7 +54,7 @@ const signIn = useCallback( async ({ email, password}) => {
         
             setData({ token, user})
 
-            toast.success(`Bem vindo ${user.name}`)
+            toast.success(`Bem vinda(a) ${user.name}.`)
         }).catch((error) => {
             let erro = JSON.parse(error.request.response)
             toast.error(erro.error)
