435993b0603a5aa63a908965df3c05846947fa28
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-01-18 19:39:33 -0300

    Adicionado font-family global

--- front-end/src/styles/global.js
+++ front-end/src/styles/global.js
@@ -16,6 +16,8 @@ export default createGlobalStyle`
     }
 
     html, body, #root {
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,500&display=swap');
+        font-family: roboto;
         height: 100%;
     }
 
